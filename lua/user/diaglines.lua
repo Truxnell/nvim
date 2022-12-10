541411477fcf4658d5bd70917a8796d009ccc628
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2022-12-11 08:11:18 +1100

    fix: add virtual line stuff

--- lua/user/diaglines.lua
+++ lua/user/diaglines.lua
@@ -1,7 +1,3 @@
-local lua_lines_ok, cmp = pcall(require, "lua_lines")
-if not lua_lines_ok then
-	return
-end
-
+-- Setup defaults for virtual text
 vim.diagnostic.config({ virtual_lines = false })
 vim.diagnostic.config({ virtual_text = true })
